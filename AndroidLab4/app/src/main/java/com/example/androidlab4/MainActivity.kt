ec6f003eaf9aa24034ea214283702464335c7cc4
Makedoncek <123456v64@gmail.com>
2024-05-29 14:01:45 +0300

    added unversioned files

---
+++ AndroidLab4/app/src/main/java/com/example/androidlab4/MainActivity.kt
@@ -0,0 +1,97 @@
+package com.example.androidlab4
+
+import android.media.MediaPlayer
+import android.net.Uri
+import android.os.Bundle
+import android.widget.Button
+import android.widget.VideoView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var mediaPlayer: MediaPlayer
+    private lateinit var videoView: VideoView
+    private var audioPaused: Boolean = false
+    private var videoPaused: Boolean = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val buttonPlayAudio = findViewById<Button>(R.id.buttonPlayAudio)
+        val buttonPauseAudio = findViewById<Button>(R.id.buttonPauseAudio)
+        val buttonStopAudio = findViewById<Button>(R.id.buttonStopAudio)
+        videoView = findViewById(R.id.videoView)
+        val buttonPlayVideo = findViewById<Button>(R.id.buttonPlayVideo)
+        val buttonPauseVideo = findViewById<Button>(R.id.buttonPauseVideo)
+        val buttonStopVideo = findViewById<Button>(R.id.buttonStopVideo)
+
+        // Аудіо-файл з внутрішнього сховища
+        val audioUri = Uri.parse("android.resource://" + packageName + "/" + R.raw.shape_of_myheart)
+        mediaPlayer = MediaPlayer.create(this, audioUri)
+
+        buttonPlayAudio.setOnClickListener {
+            if (audioPaused) {
+                mediaPlayer.start()
+                audioPaused = false
+            } else {
+                mediaPlayer = MediaPlayer.create(this, audioUri)
+                mediaPlayer.start()
+            }
+        }
+
+        buttonPauseAudio.setOnClickListener {
+            if (mediaPlayer.isPlaying) {
+                mediaPlayer.pause()
+                audioPaused = true
+            }
+        }
+
+        buttonStopAudio.setOnClickListener {
+            if (mediaPlayer.isPlaying || audioPaused) {
+                mediaPlayer.stop()
+                mediaPlayer.release()
+                audioPaused = false
+            }
+        }
+
+        // Відео-файл з внутрішнього сховища
+        val videoUri = Uri.parse("android.resource://" + packageName + "/" + R.raw.pes_patron)
+        videoView.setVideoURI(videoUri)
+
+        buttonPlayVideo.setOnClickListener {
+            if (videoPaused) {
+                videoView.start()
+                videoPaused = false
+            } else {
+                videoView.setVideoURI(videoUri)
+                videoView.start()
+            }
+        }
+
+        buttonPauseVideo.setOnClickListener {
+            if (videoView.isPlaying) {
+                videoView.pause()
+                videoPaused = true
+            }
+        }
+
+        buttonStopVideo.setOnClickListener {
+            if (videoView.isPlaying || videoPaused) {
+                videoView.stopPlayback()
+                videoPaused = false
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (mediaPlayer.isPlaying) {
+            mediaPlayer.stop()
+            mediaPlayer.release()
+        }
+        if (videoView.isPlaying) {
+            videoView.stopPlayback()
+        }
+    }
+}
